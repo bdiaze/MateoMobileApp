2d8506ecf93e3ee31bbbf6b0170bad6cd6dbb7d6
Benjamín <benjamin.diaze@outlook.com>
2025-01-26 12:44:51 -0300

    Se modifica animación de fondo y font del cronometro, se crean iconos de la aplicación.

--- lib/src/contador_series/presentation/cronometro.dart
+++ lib/src/contador_series/presentation/cronometro.dart
@@ -10,83 +10,63 @@ class Cronometro extends StatefulWidget {
 }
 
 class _CronometroState extends State<Cronometro> with SingleTickerProviderStateMixin {
-
   final int maxMinutos = 999;
-  final Map<int, int> alertasDescanso = {
-    110: 1, // Cuando queden 10 segundos para los 2 minutos de descanso se manda
-    115: 2, // Cuando queden 5 segundos para los 2 minutos de descanso
-    120: 3 // Cuando se cumplan los 2 minutos
+  final Map<int, Map> alertasDescanso = {
+    110: { 'cantRepeticiones': 1, 'cambioPermanente': false }, // Cuando queden 10 segundos para los 2 minutos de descanso se manda
+    120: { 'cantRepeticiones': 2, 'cambioPermanente': false } // Cuando se cumplan los 2 minutos
   };
-
-  final coloresFondo = <TweenSequenceItem<Color?>>[
-    TweenSequenceItem(
-      tween: ColorTween(begin: Colors.blue[800], end: Colors.red[800]),
-      weight: 1.0,
-    ),
-    TweenSequenceItem(
-      tween: ColorTween(begin: Colors.red[800], end: Colors.blue[800]),
-      weight: 1.0,
-    ),
-  ];
-
-  final coloresLetras = <TweenSequenceItem<Color?>>[
-    TweenSequenceItem(
-      tween: ColorTween(begin: Colors.blue[600], end: Colors.red[600]),
-      weight: 1.0,
-    ),
-    TweenSequenceItem(
-      tween: ColorTween(begin: Colors.red[600], end: Colors.blue[600]),
-      weight: 1.0,
-    ),
-  ];
 
   int tipoEjecucion = 0; // 0: Aún no ejecuta - 1: Contando Serie - 2: Contando Descando
   int tiempo = 0;
   Timer? timer;
+  Color? colorFondo = Colors.blue[800];
 
-  AnimationController? controladorAnimacionAlerta;
-  Animation<Color?>? animacionColorFondo;
-  Animation<Color?>? animacionColorLetras;
+  AnimationController? controladorAnimacionFondo;
+  Animation<Color?>? animacionFondo;
 
   @override
   void initState() {
     super.initState();
-    controladorAnimacionAlerta = AnimationController(
-        duration: Duration(milliseconds: 750),
-        vsync: this
-    );
-
-    animacionColorFondo = TweenSequence<Color?>(coloresFondo).animate(controladorAnimacionAlerta!);
-    animacionColorLetras = TweenSequence<Color?>(coloresLetras).animate(controladorAnimacionAlerta!);
+    controladorAnimacionFondo = AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    controladorAnimacionFondo!.addListener(() {
+      setState(() {
+        colorFondo = animacionFondo!.value;
+      });
+    });
   }
 
-  void gatillarAlertaDescanso(int tipoAlerta) {
-    switch (tipoAlerta) {
-      case 1: // Alerta Liviana
-        Vibration.vibrate(duration: 500);
-        controladorAnimacionAlerta!.reset();
-        controladorAnimacionAlerta!.repeat(
-          period: Duration(milliseconds: 500),
-          count: 1
-        );
-        break;
-      case 2: // Alerta Mediana
-        Vibration.vibrate(duration: 1000);
-        controladorAnimacionAlerta!.reset();
-        controladorAnimacionAlerta!.repeat(
-            period: Duration(milliseconds: 500),
-            count: 2
-        );
-        break;
-      case 3: // Alerta Fuerte
-        Vibration.vibrate(duration: 1500);
-        controladorAnimacionAlerta!.reset();
-        controladorAnimacionAlerta!.repeat(
-            period: Duration(milliseconds: 500),
-            count: 3
-        );
-        break;
+  void gatillarCambioFondo({ required List<Color?> nuevosColores, int duracion = 500, int repeticiones = 1}) {
+    Color? colorAnterior = colorFondo;
+    List<TweenSequenceItem<Color?>> tweenlist = [];
+    for (Color? nuevoColor in nuevosColores) {
+      tweenlist.add(TweenSequenceItem(tween: ColorTween(begin: colorAnterior, end: nuevoColor), weight: 1.0,));
+      colorAnterior = nuevoColor;
     }
+    animacionFondo = TweenSequence<Color?>(tweenlist).animate(controladorAnimacionFondo!);
+    controladorAnimacionFondo!.duration = Duration(milliseconds: duracion);
+    controladorAnimacionFondo!.reset();
+    controladorAnimacionFondo!.forward();
+  }
+
+  void gatillarAlertaDescanso(int cantRepeticiones, bool cambioPermanente) {
+    Vibration.vibrate(duration: 500 * cantRepeticiones);
+
+    List<Color?> listaColores= [];
+    for (int i = 0; i < cantRepeticiones; i++) {
+      listaColores.add(Colors.red[900]);
+      listaColores.add(colorFondo);
+    }
+
+    int deltaTiempoCambioPermanente = 0;
+    if (cambioPermanente) {
+      listaColores.add(Colors.red[900]);
+      deltaTiempoCambioPermanente = 250;
+    }
+
+    gatillarCambioFondo(
+      nuevosColores: listaColores,
+      duracion: 500 * cantRepeticiones + deltaTiempoCambioPermanente,
+    );
   }
 
   String tiempoContadorToString() {
@@ -96,10 +76,17 @@ class _CronometroState extends State<Cronometro> with SingleTickerProviderStateM
   }
 
   void clickBotonCronometro() {
+    int tipoEjecucionAnterior = tipoEjecucion;
     setState(() {
       tipoEjecucion = tipoEjecucion != 2 ? tipoEjecucion + 1 : 1;
       tiempo = 0;
     });
+    
+    if (tipoEjecucion == 2) {
+      gatillarCambioFondo(nuevosColores: [Colors.grey[800]]);
+    } else if (tipoEjecucion == 1 && tipoEjecucionAnterior == 2) {
+      gatillarCambioFondo(nuevosColores: [Colors.blue[800]]);
+    }
 
     if (timer != null && timer!.isActive) {
       timer!.cancel();
@@ -112,7 +99,7 @@ class _CronometroState extends State<Cronometro> with SingleTickerProviderStateM
       });
       // Si estamos en un descanso y el tiempo que llevamos requiere una alerta, se gatilla la alerta
       if (tipoEjecucion == 2 && alertasDescanso.containsKey(tiempo)) {
-        gatillarAlertaDescanso(alertasDescanso[tiempo]!);
+        gatillarAlertaDescanso(alertasDescanso[tiempo]!['cantRepeticiones']!, alertasDescanso[tiempo]!['cambioPermanente']!);
       }
     });
   }
@@ -121,85 +108,81 @@ class _CronometroState extends State<Cronometro> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     WakelockPlus.enable();
 
-    return AnimatedBuilder(
-      animation: controladorAnimacionAlerta!,
-      builder: (BuildContext context, _) {
-        return Scaffold(
-          backgroundColor: animacionColorFondo!.value,
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    if (tipoEjecucion == 1)
-                      Center(
-                        child: Image.asset(
-                          'assets/images/musculo.png',
-                          color: Colors.grey[300],
-                          height: 140,
-                        ),
-                      ),
-                    if (tipoEjecucion == 2)
-                      Center(
-                        child: Image.asset(
-                          'assets/images/zzz.png',
-                          color: Colors.grey[300],
-                          height: 140,
-                        ),
-                      ),
-                    if ([1,2].contains(tipoEjecucion))
-                      SizedBox(height: 20.0),
-                    if (tipoEjecucion != 0)
-                      Center(
-                        child: Text(
-                          tiempoContadorToString(),
-                          style: TextStyle(
-                              fontSize: 90.0,
-                              color: Colors.grey[300]
-                          ),
-                        ),
-                      ),
-                    if (tipoEjecucion != 0)
-                      SizedBox(height: 30.0),
-                    Center(
-                      child: FilledButton.icon(
-                          onPressed: clickBotonCronometro,
-                          icon: Icon(
-                              [0, 2].contains(tipoEjecucion) ? Icons.play_arrow : Icons.pause,
-                              color: animacionColorLetras!.value,
-                              size: 40
-                          ),
-                          label: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(
-                              [0, 2].contains(tipoEjecucion) ? '¡A Entrenar!' : 'Descanso',
-                              style: TextStyle(
-                                fontSize: 25.0,
-                                color: animacionColorLetras!.value,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          style: FilledButton.styleFrom(
-                            backgroundColor: Colors.grey[300],
-                          )
+    return Scaffold(
+      backgroundColor: colorFondo,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                if (tipoEjecucion == 1)
+                  Center(
+                    child: Image.asset(
+                      'assets/images/contador_series/musculo.png',
+                      color: Colors.grey[300],
+                      height: 140,
+                    ),
+                  ),
+                if (tipoEjecucion == 2)
+                  Center(
+                    child: Image.asset(
+                      'assets/images/contador_series/zzz.png',
+                      color: Colors.grey[300],
+                      height: 140,
+                    ),
+                  ),
+                if ([1,2].contains(tipoEjecucion))
+                  SizedBox(height: 20.0),
+                if (tipoEjecucion != 0)
+                  Center(
+                    child: Text(
+                      tiempoContadorToString(),
+                      style: TextStyle(
+                        fontSize: 90.0,
+                        color: Colors.grey[300],
+                        fontFamily: 'RobotoMono'
                       ),
                     ),
-                    if ([1,2].contains(tipoEjecucion))
-                      SizedBox(height: 70.0),
-                  ]
-              ),
-            ),
+                  ),
+                if (tipoEjecucion != 0)
+                  SizedBox(height: 30.0),
+                Center(
+                  child: FilledButton.icon(
+                      onPressed: clickBotonCronometro,
+                      icon: Icon(
+                          [0, 2].contains(tipoEjecucion) ? Icons.play_arrow : Icons.pause,
+                          color: colorFondo,
+                          size: 40
+                      ),
+                      label: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          [0, 2].contains(tipoEjecucion) ? '¡A Entrenar!' : 'Descanso',
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            color: colorFondo,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      style: FilledButton.styleFrom(
+                        backgroundColor: Colors.grey[300],
+                      )
+                  ),
+                ),
+                if ([1,2].contains(tipoEjecucion))
+                  SizedBox(height: 70.0),
+              ]
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 
   @override
   void dispose() {
-    controladorAnimacionAlerta!.dispose();
+    controladorAnimacionFondo!.dispose();
     WakelockPlus.disable();
     super.dispose();
   }
